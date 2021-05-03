841892ff1c60d402f1e40a265f273619837d6381
Syafii Ghazali <asy-syafii.ghazali@capgemini.com>
2021-05-03 18:41:53 +0800

    Added autofac DI

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() 
+            { 
+                new Restaurant {Id = 1, Name = "Denny's", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Wendy's", Cuisine = CuisineType.French},
+                new Restaurant {Id = 3, Name = "Syafii's", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 4, Name = "Randy's", Cuisine = CuisineType.None}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
